8b24029f84105050d9e292ab2836365306bcbd0e
araragi123 <santiago.gar729@gmail.com>
2026-05-13 22:07:35 -0500

    Remove comment from calcularSalario method

--- src/model/Tester.cpp
+++ src/model/Tester.cpp
@@ -4,7 +4,7 @@ Tester::Tester(string nombre, float salarioBase)
     : Empleado(nombre, salarioBase) {}
 
 float Tester::calcularSalario() {
-    return salarioBase * 1.05f; // bono 5%
+    return salarioBase * 1.05f; 
 }
 
 string Tester::getTipo() {
